faf867731eded49201374d2af437d9de2ea829dd
mannireis <itsmannireis@gmail.com>
2021-10-17 15:24:41 +0100

    not special

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,6 @@
-// strings
-console.log("hello world");
 
 let email = "itsmannireis@gmail.com";
-console.log(email);
-// string conca
-let firstname = "manni";
-let lastname = "reis";
 
-let fullname = firstname + " " + lastname;
-console.log(fullname);
- // get chara
- console.log(fullname[0]);
+ 
 
- //st learning
-console.log(fullname.length);
-// st meth
-console.log(fullname.toUpperCase);
+console.log(result);
